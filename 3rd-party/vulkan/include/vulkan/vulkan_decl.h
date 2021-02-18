903cea3c38064a900b0a0f804ac597003f2d9de9
MikhailGorobets <misha.friman@mail.ru>
2021-02-18 05:34:27 +0500

    Sepate dxc compiler and vulkan_decl.h

--- 3rd-party/vulkan/include/vulkan/vulkan_decl.h
+++ 3rd-party/vulkan/include/vulkan/vulkan_decl.h
@@ -1,22 +1,13 @@
 #pragma once
 
 #define VK_NO_PROTOTYPES 
-#define VMA_DYNAMIC_VULKAN_FUNCTIONS 0
-#define VMA_STATIC_VULKAN_FUNCTIONS  0
 #define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
 
 #include <vulkan/vulkan.hpp>
 #include <vulkan/vk_mem_alloc.hpp>
 
-#include <spirv/spirv_cross.hpp>
-#include <spirv/spirv_parser.hpp>
-
-#include <smolv/smolv.h>
-#include <dxc/dxcapi.use.h>
-
 #include <fmt/core.h>
 #include <fmt/format.h>
-#include <optional>
 
 namespace vkx {
 
@@ -45,7 +36,7 @@ namespace vkx {
     }
 
     template<typename T>
-    auto setDebugName(vk::Device device, T objectHandle, std::string const& objectName) -> void {
+    auto setDebugName(vk::Device device, T objectHandle, std::string const& objectName) -> void {     
     #ifdef _DEBUG
         auto objectNameCombine = fmt::format("{0}: {1}", vk::to_string(T::objectType), objectName);
         device.setDebugUtilsObjectNameEXT(vk::DebugUtilsObjectNameInfoEXT{ .objectType = T::objectType, .objectHandle = reinterpret_cast<uint64_t&>(objectHandle), .pObjectName = objectNameCombine.c_str() });
@@ -86,265 +77,6 @@ namespace vkx {
 }
 
 
-
-namespace vkx {
-
-    class ComException : public std::exception {
-    public:
-        ComException(HRESULT hr) : result(hr) {}
-
-        const char* what() const override {
-            static char s_str[64] = {};
-            ::sprintf_s(s_str, "Failure with HRESULT of %08X", static_cast<uint32_t>(result));
-            return s_str;
-        }
-
-    private:
-        HRESULT result;
-    };
-
-    inline auto throwIfFailed(HRESULT hr) -> void {
-        if (FAILED(hr)) throw ComException(hr);
-    }
-
-    template<typename T>
-    struct ComPtr {
-        ComPtr(T* pComObj = nullptr) : m_pComObj(pComObj) {
-            static_assert(std::is_base_of<IUnknown, T>::value, "T needs to be IUnknown based");
-            if (m_pComObj)
-                m_pComObj->AddRef();
-        }
-
-        ComPtr(const ComPtr<T>& pComObj) {
-            static_assert(std::is_base_of<IUnknown, T>::value, "T needs to be IUnknown based");
-            m_pComObj = pComObj.m_pComObj;
-            if (m_pComObj)
-                m_pComObj->AddRef();
-        }
-
-        ComPtr(ComPtr<T>&& pComObj) {
-            m_pComObj = pComObj.m_pComObj;
-            pComObj.m_pComObj = nullptr;
-        }
-
-        T* operator=(T* pComObj) {
-            if (m_pComObj)
-                m_pComObj->Release();
-
-            m_pComObj = pComObj;
-
-            if (m_pComObj)
-                m_pComObj->AddRef();
-
-            return m_pComObj;
-        }
-
-        T* operator=(const ComPtr<T>& pComObj) {
-            if (m_pComObj)
-                m_pComObj->Release();
-
-            m_pComObj = pComObj.m_pComObj;
-
-            if (m_pComObj)
-                m_pComObj->AddRef();
-
-            return m_pComObj;
-        }
-
-        ~ComPtr() {
-            if (m_pComObj) {
-                m_pComObj->Release();
-                m_pComObj = nullptr;
-            }
-        }
-
-        operator T* () const {
-            return m_pComObj;
-        }
-
-
-        T& operator*() const {
-            return *m_pComObj;
-        }
-
-        T** operator&() {
-            assert(nullptr == m_pComObj);
-            return &m_pComObj;
-        }
-
-        T* operator->() const {
-            return m_pComObj;
-        }
-
-        bool operator!() const {
-            return (nullptr == m_pComObj);
-        }
-
-        bool operator<(T* pComObj) const {
-            return m_pComObj < pComObj;
-        }
-
-        bool operator!=(T* pComObj) const {
-            return !operator==(pComObj);
-        }
-
-        bool operator==(T* pComObj) const {
-            return m_pComObj == pComObj;
-        }
-
-        template <typename I>
-        HRESULT queryInterface(I** ppInterface) {
-            return m_pComObj->QueryInterface(IID_PPV_ARGS(ppInterface));
-        }
-
-    protected:
-        T* m_pComObj;
-    };
-
-    class Compiler;
-    class CompileResult {
-        friend Compiler;
-        CompileResult(std::vector<uint32_t>&& data) : m_Data(data) {}
-
-    public:
-        [[nodiscard]] auto data() const -> const uint32_t* {
-            return std::data(m_Data);
-        }
-        [[nodiscard]] auto size() const -> uint32_t {
-            return static_cast<uint32_t>(std::size(m_Data) * sizeof(uint32_t));
-        }
-    private:
-        std::vector<uint32_t> m_Data;
-    };
-
-    struct CompilerCreateInfo {
-        uint32_t shaderModelMajor = 6;
-        uint32_t shaderModelMinor = 5;
-        uint32_t HLSLVersion = 2018;
-    };
-
-    //class CompilerFactory {
-    //public:
-    //    auto createCompiler(CompilerCreateInfo const& compilerCreateInfo) -> Compiler {
-    //        std::scoped_lock<std::mutex> lock(m_Mutex);
-    //
-    //       // throwIfFailed(m_DxcLoader.Initialize());
-    //       // throwIfFailed(m_DxcLoader.CreateInstance(CLSID_DxcUtils, &m_pDxcUtils));
-    //       // throwIfFailed(m_DxcLoader.CreateInstance(CLSID_DxcCompiler, &m_pDxcCompiler));
-    //       // throwIfFailed(m_pDxcUtils->CreateDefaultIncludeHandler(&m_pDxcIncludeHandler));
-    //
-    //
-    //    }
-    //    auto destroyCompiler(Compiler handle) -> void {
-    //
-    //    }
-    //private:
-    //    std::mutex m_Mutex;
-    //};
-
-    class Compiler {
-    public:
-        Compiler(CompilerCreateInfo const& compilerCreateInfo) {
-
-            m_CompilerDesc = compilerCreateInfo;
-            throwIfFailed(m_DxcLoader.Initialize());
-            throwIfFailed(m_DxcLoader.CreateInstance(CLSID_DxcUtils, &m_pDxcUtils));
-            throwIfFailed(m_DxcLoader.CreateInstance(CLSID_DxcCompiler, &m_pDxcCompiler));
-            throwIfFailed(m_pDxcUtils->CreateDefaultIncludeHandler(&m_pDxcIncludeHandler));
-
-        }
-
-        [[nodiscard]] auto compileFromString(std::wstring const& data, std::wstring const& entryPoint, vk::ShaderStageFlagBits target, std::vector<std::wstring> const& defines) const -> CompileResult {
-            ComPtr<IDxcBlobEncoding> pDxcSource;
-            throwIfFailed(m_pDxcUtils->CreateBlob(std::data(data), static_cast<uint32_t>(std::size(data)), CP_UTF8, &pDxcSource));
-            return compileShaderBlob(pDxcSource, entryPoint, target, defines);
-        }
-
-        [[nodiscard]] auto compileFromFile(std::wstring const& path, std::wstring const& entryPoint, vk::ShaderStageFlagBits target, std::vector<std::wstring> const& defines) const -> CompileResult {
-            ComPtr<IDxcBlobEncoding> pDxcSource;
-            throwIfFailed(m_pDxcUtils->LoadFile(path.c_str(), nullptr, &pDxcSource));
-            return compileShaderBlob(pDxcSource, entryPoint, target, defines);
-        }
-
-    private:
-        [[nodiscard]] auto compileShaderBlob(ComPtr<IDxcBlobEncoding> pDxcBlob, std::wstring const& entryPoint, vk::ShaderStageFlagBits target, std::vector<std::wstring> const& defines) const -> CompileResult {
-
-            auto const getShaderStage = [](vk::ShaderStageFlagBits target) -> std::wstring {
-                switch (target) {
-                case vk::ShaderStageFlagBits::eVertex:                 return L"vs";
-                case vk::ShaderStageFlagBits::eGeometry:               return L"gs";
-                case vk::ShaderStageFlagBits::eTessellationControl:    return L"hs";
-                case vk::ShaderStageFlagBits::eTessellationEvaluation: return L"ds";
-                case vk::ShaderStageFlagBits::eFragment:               return L"ps";
-                case vk::ShaderStageFlagBits::eCompute:                return L"cs";
-                default: vk::throwResultException(vk::Result::eErrorUnknown, "Unknown shader type");
-                }
-            };
-
-            const std::wstring shaderProfile = fmt::format(L"{0}_{1}_{2}", getShaderStage(target), m_CompilerDesc.shaderModelMajor, m_CompilerDesc.shaderModelMinor);
-            const std::wstring shaderVersion = std::to_wstring(m_CompilerDesc.HLSLVersion);
-
-            std::vector<LPCWSTR> dxcArguments;
-            dxcArguments.insert(std::end(dxcArguments), { L"-E",  entryPoint.c_str() });
-            dxcArguments.insert(std::end(dxcArguments), { L"-T",  shaderProfile.c_str() });
-            dxcArguments.insert(std::end(dxcArguments), { L"-HV", shaderVersion.c_str() });
-
-            dxcArguments.insert(std::end(dxcArguments), { L"-spirv", L"-fspv-reflect", L"-fspv-target-env=vulkan1.1", L"-enable-16bit-types" });
-#ifdef  _DEBUG
-            dxcArguments.insert(std::end(dxcArguments), { L"-fspv-debug=file", L"-fspv-debug=source",  L"-fspv-debug=line",  L"-fspv-debug=tool" });
-#endif
-
-            for (auto const& e : defines)
-                dxcArguments.insert(std::end(dxcArguments), { L"-D", e.c_str() });
-
-            DxcText dxcBuffer = {};
-            dxcBuffer.Ptr = pDxcBlob->GetBufferPointer();
-            dxcBuffer.Size = pDxcBlob->GetBufferSize();
-
-            ComPtr<IDxcResult> pDxcCompileResult;
-            throwIfFailed(m_pDxcCompiler->Compile(&dxcBuffer, std::data(dxcArguments), static_cast<uint32_t>(std::size(dxcArguments)), m_pDxcIncludeHandler, IID_PPV_ARGS(&pDxcCompileResult)));
-
-            ComPtr<IDxcBlobUtf8> pDxcErrors;
-            throwIfFailed(pDxcCompileResult->GetOutput(DXC_OUT_ERRORS, IID_PPV_ARGS(&pDxcErrors), nullptr));
-            if (pDxcErrors && pDxcErrors->GetStringLength() > 0)
-                fmt::print("{}\n", static_cast<const char*>(pDxcErrors->GetBufferPointer()));
-
-
-            ComPtr<IDxcBlob> pDxcShaderCode;
-            throwIfFailed(pDxcCompileResult->GetOutput(DXC_OUT_OBJECT, IID_PPV_ARGS(&pDxcShaderCode), nullptr));
-
-            auto spirv = std::vector<uint32_t>(static_cast<uint32_t*>(pDxcShaderCode->GetBufferPointer()), static_cast<uint32_t*>(pDxcShaderCode->GetBufferPointer()) + pDxcShaderCode->GetBufferSize() / sizeof(uint32_t));
-            return CompileResult(std::move(spirv));
-        }
-
-    private:
-        dxc::DxcDllSupport         m_DxcLoader;
-        ComPtr<IDxcUtils>          m_pDxcUtils;
-        ComPtr<IDxcCompiler3>      m_pDxcCompiler;
-        ComPtr<IDxcIncludeHandler> m_pDxcIncludeHandler;
-        CompilerCreateInfo         m_CompilerDesc;
-    };
-}
-
-
-namespace vkx {
-
-    struct SpirvShaderResourceAttributes {
-        vk::DescriptorType descriptorType;
-        uint32_t           descriptorCount;
-        std::string        descriptorName;
-    };
-
-    struct SpirvShaderStageInputAttributes {
-
-    };
-
-    struct SpirvShaderResources {
-
-    };
-
-}
-
 namespace vkx {
 
     struct ImageTransition {
@@ -369,7 +101,6 @@ namespace vkx {
     void imageTransition(vk::CommandBuffer cmdBuffer, ImageTransition const& translation);
 
     void bufferTransition(vk::CommandBuffer cmdBuffer, BufferTransition const& translation);
-
 }
 
 
--- source/Main.cpp
+++ source/Main.cpp
@@ -1,10 +1,11 @@
 #include <vulkan/vulkan_decl.h>
 
+#include <dxc/dxcapi.use.h>
+
 #include <imgui/imgui.h>
 #include <imgui/imgui_impl_glfw.h>
 #include <imgui/imgui_impl_vulkan.h>
 #include <implot/implot.h>
-
 
 #include <glfw/glfw3.h>
 #include <glfw/glfw3native.h>
@@ -20,6 +21,7 @@
 #include <numeric>
 #include <sstream>
 #include <mutex>
+
 
 struct ScrollingBuffer {
     int32_t MaxSize = 4096;
@@ -179,10 +181,315 @@ class MemoryStatisticCPU {
 };
 
 
+namespace vkx {
+
+    class ComException : public std::exception {
+    public:
+        ComException(HRESULT hr) : result(hr) {}
+
+        const char* what() const override {
+            static char s_str[64] = {};
+            ::sprintf_s(s_str, "Failure with HRESULT of %08X", static_cast<uint32_t>(result));
+            return s_str;
+        }
+
+    private:
+        HRESULT result;
+    };
+
+    inline auto throwIfFailed(HRESULT hr) -> void {
+        if (FAILED(hr)) throw ComException(hr);
+    }
+
+    template<typename T>
+    struct ComPtr {
+        ComPtr(T* pComObj = nullptr) : m_pComObj(pComObj) {
+            static_assert(std::is_base_of<IUnknown, T>::value, "T needs to be IUnknown based");
+            if (m_pComObj)
+                m_pComObj->AddRef();
+        }
+
+        ComPtr(const ComPtr<T>& pComObj) {
+            static_assert(std::is_base_of<IUnknown, T>::value, "T needs to be IUnknown based");
+            m_pComObj = pComObj.m_pComObj;
+            if (m_pComObj)
+                m_pComObj->AddRef();
+        }
+
+        ComPtr(ComPtr<T>&& pComObj) {
+            m_pComObj = pComObj.m_pComObj;
+            pComObj.m_pComObj = nullptr;
+        }
+
+        T* operator=(T* pComObj) {
+            if (m_pComObj)
+                m_pComObj->Release();
+
+            m_pComObj = pComObj;
+
+            if (m_pComObj)
+                m_pComObj->AddRef();
+
+            return m_pComObj;
+        }
+
+        T* operator=(const ComPtr<T>& pComObj) {
+            if (m_pComObj)
+                m_pComObj->Release();
+
+            m_pComObj = pComObj.m_pComObj;
+
+            if (m_pComObj)
+                m_pComObj->AddRef();
+
+            return m_pComObj;
+        }
+
+        ~ComPtr() {
+            if (m_pComObj) {
+                m_pComObj->Release();
+                m_pComObj = nullptr;
+            }
+        }
+
+        operator T* () const {
+            return m_pComObj;
+        }
+
+
+        T& operator*() const {
+            return *m_pComObj;
+        }
+
+        T** operator&() {
+            assert(nullptr == m_pComObj);
+            return &m_pComObj;
+        }
+
+        T* operator->() const {
+            return m_pComObj;
+        }
+
+        bool operator!() const {
+            return (nullptr == m_pComObj);
+        }
+
+        bool operator<(T* pComObj) const {
+            return m_pComObj < pComObj;
+        }
+
+        bool operator!=(T* pComObj) const {
+            return !operator==(pComObj);
+        }
+
+        bool operator==(T* pComObj) const {
+            return m_pComObj == pComObj;
+        }
+
+        template <typename I>
+        HRESULT queryInterface(I** ppInterface) {
+            return m_pComObj->QueryInterface(IID_PPV_ARGS(ppInterface));
+        }
+
+    protected:
+        T* m_pComObj;
+    };
+
+    class Compiler;
+    class CompileResult {
+        friend Compiler;
+        CompileResult(std::vector<uint32_t>&& data) : m_Data(data) {}
+
+    public:
+        [[nodiscard]] auto data() const -> const uint32_t* {
+            return std::data(m_Data);
+        }
+        [[nodiscard]] auto size() const -> uint32_t {
+            return static_cast<uint32_t>(std::size(m_Data) * sizeof(uint32_t));
+        }
+        
+        [[nodiscard]] auto operator[](size_t index) const -> uint32_t { 
+            return this->data()[index];
+        }
+
+    private:
+        std::vector<uint32_t> m_Data;
+    };
+
+    struct CompilerCreateInfo {
+        uint32_t shaderModelMajor = 6;
+        uint32_t shaderModelMinor = 5;
+        uint32_t HLSLVersion = 2018;
+    };
+
+    //class CompilerFactory {
+    //public:
+    //    auto createCompiler(CompilerCreateInfo const& compilerCreateInfo) -> Compiler {
+    //        std::scoped_lock<std::mutex> lock(m_Mutex);
+    //
+    //       // throwIfFailed(m_DxcLoader.Initialize());
+    //       // throwIfFailed(m_DxcLoader.CreateInstance(CLSID_DxcUtils, &m_pDxcUtils));
+    //       // throwIfFailed(m_DxcLoader.CreateInstance(CLSID_DxcCompiler, &m_pDxcCompiler));
+    //       // throwIfFailed(m_pDxcUtils->CreateDefaultIncludeHandler(&m_pDxcIncludeHandler));
+    //
+    //
+    //    }
+    //    auto destroyCompiler(Compiler handle) -> void {
+    //
+    //    }
+    //private:
+    //    std::mutex m_Mutex;
+    //};
+
+    class Compiler {
+    public:
+        Compiler(CompilerCreateInfo const& compilerCreateInfo) {
+
+            m_CompilerDesc = compilerCreateInfo;
+            throwIfFailed(m_DxcLoader.Initialize());
+            throwIfFailed(m_DxcLoader.CreateInstance(CLSID_DxcUtils, &m_pDxcUtils));
+            throwIfFailed(m_DxcLoader.CreateInstance(CLSID_DxcCompiler, &m_pDxcCompiler));
+            throwIfFailed(m_pDxcUtils->CreateDefaultIncludeHandler(&m_pDxcIncludeHandler));
+
+        }
+
+        [[nodiscard]] auto compileFromString(std::wstring const& data, std::wstring const& entryPoint, vk::ShaderStageFlagBits target, std::vector<std::wstring> const& defines) const -> CompileResult {
+            ComPtr<IDxcBlobEncoding> pDxcSource;
+            throwIfFailed(m_pDxcUtils->CreateBlob(std::data(data), static_cast<uint32_t>(std::size(data)), CP_UTF8, &pDxcSource));
+            return compileShaderBlob(pDxcSource, entryPoint, target, defines);
+        }
+
+        [[nodiscard]] auto compileFromFile(std::wstring const& path, std::wstring const& entryPoint, vk::ShaderStageFlagBits target, std::vector<std::wstring> const& defines) const -> CompileResult {
+            ComPtr<IDxcBlobEncoding> pDxcSource;
+            throwIfFailed(m_pDxcUtils->LoadFile(path.c_str(), nullptr, &pDxcSource));
+            return compileShaderBlob(pDxcSource, entryPoint, target, defines);
+        }
+
+    private:
+        [[nodiscard]] auto compileShaderBlob(ComPtr<IDxcBlobEncoding> pDxcBlob, std::wstring const& entryPoint, vk::ShaderStageFlagBits target, std::vector<std::wstring> const& defines) const -> CompileResult {
+
+            auto const getShaderStage = [](vk::ShaderStageFlagBits target) -> std::wstring {
+                switch (target) {
+                case vk::ShaderStageFlagBits::eVertex:                 return L"vs";
+                case vk::ShaderStageFlagBits::eGeometry:               return L"gs";
+                case vk::ShaderStageFlagBits::eTessellationControl:    return L"hs";
+                case vk::ShaderStageFlagBits::eTessellationEvaluation: return L"ds";
+                case vk::ShaderStageFlagBits::eFragment:               return L"ps";
+                case vk::ShaderStageFlagBits::eCompute:                return L"cs";
+                default: vk::throwResultException(vk::Result::eErrorUnknown, "Unknown shader type");
+                }
+            };
+
+            const std::wstring shaderProfile = fmt::format(L"{0}_{1}_{2}", getShaderStage(target), m_CompilerDesc.shaderModelMajor, m_CompilerDesc.shaderModelMinor);
+            const std::wstring shaderVersion = std::to_wstring(m_CompilerDesc.HLSLVersion);
+
+            std::vector<LPCWSTR> dxcArguments;
+            dxcArguments.insert(std::end(dxcArguments), { L"-E",  entryPoint.c_str() });
+            dxcArguments.insert(std::end(dxcArguments), { L"-T",  shaderProfile.c_str() });
+            dxcArguments.insert(std::end(dxcArguments), { L"-HV", shaderVersion.c_str() });
+
+            dxcArguments.insert(std::end(dxcArguments), { L"-spirv", L"-fspv-reflect", L"-fspv-target-env=vulkan1.1", L"-enable-16bit-types" });
+#ifdef  _DEBUG
+            dxcArguments.insert(std::end(dxcArguments), { L"-fspv-debug=file", L"-fspv-debug=source",  L"-fspv-debug=line",  L"-fspv-debug=tool" });
+#endif
+
+            for (auto const& e : defines)
+                dxcArguments.insert(std::end(dxcArguments), { L"-D", e.c_str() });
+
+            DxcText dxcBuffer = {};
+            dxcBuffer.Ptr = pDxcBlob->GetBufferPointer();
+            dxcBuffer.Size = pDxcBlob->GetBufferSize();
+
+            ComPtr<IDxcResult> pDxcCompileResult;
+            throwIfFailed(m_pDxcCompiler->Compile(&dxcBuffer, std::data(dxcArguments), static_cast<uint32_t>(std::size(dxcArguments)), m_pDxcIncludeHandler, IID_PPV_ARGS(&pDxcCompileResult)));
+
+            ComPtr<IDxcBlobUtf8> pDxcErrors;
+            throwIfFailed(pDxcCompileResult->GetOutput(DXC_OUT_ERRORS, IID_PPV_ARGS(&pDxcErrors), nullptr));
+            if (pDxcErrors && pDxcErrors->GetStringLength() > 0)
+                fmt::print("{}\n", static_cast<const char*>(pDxcErrors->GetBufferPointer()));
+
+
+            ComPtr<IDxcBlob> pDxcShaderCode;
+            throwIfFailed(pDxcCompileResult->GetOutput(DXC_OUT_OBJECT, IID_PPV_ARGS(&pDxcShaderCode), nullptr));
+
+            auto spirv = std::vector<uint32_t>(static_cast<uint32_t*>(pDxcShaderCode->GetBufferPointer()), static_cast<uint32_t*>(pDxcShaderCode->GetBufferPointer()) + pDxcShaderCode->GetBufferSize() / sizeof(uint32_t));
+            return CompileResult(std::move(spirv));
+        }
+
+    private:
+        dxc::DxcDllSupport         m_DxcLoader;
+        ComPtr<IDxcUtils>          m_pDxcUtils;
+        ComPtr<IDxcCompiler3>      m_pDxcCompiler;
+        ComPtr<IDxcIncludeHandler> m_pDxcIncludeHandler;
+        CompilerCreateInfo         m_CompilerDesc;
+    };
+}
+
+
+namespace vkx {
+
+    struct SpirvShaderResourceAttributes {
+        vk::DescriptorType descriptorType;
+        uint32_t           descriptorCount;
+        std::string        descriptorName;
+    };
+
+    struct SpirvShaderStageInputAttributes {
+
+    };
+
+    struct SpirvShaderResources {
+
+    };
+
+}
+
+
+
+//auto GenerateCPPFromShaderModule(std::string const& fileName, std::wstring const& path, std::wstring const& kernel, vk::ShaderStageFlagBits stage, std::vector<std::wstring> const& defines) {
+//    vkx::Compiler compiler(vkx::CompilerCreateInfo{.shaderModelMajor = 6, .shaderModelMinor = 5, .HLSLVersion = 2018});
+//    auto const spirv = compiler.compileFromFile(path, kernel, stage, defines);
+//
+//
+//    auto const GetShaderStage = [](vk::ShaderStageFlagBits target) -> const char* {
+//        switch (target) {
+//        case vk::ShaderStageFlagBits::eVertex:
+//            return "VertexShader_SPIRV";
+//        case vk::ShaderStageFlagBits::eGeometry:
+//            return "GeometryShader_SPIRV";
+//        case vk::ShaderStageFlagBits::eTessellationControl:
+//            return "TessellationControlhader_SPIRV";
+//        case vk::ShaderStageFlagBits::eTessellationEvaluation:
+//            return "TessellationEvaluationShader_SPIRV";
+//        case vk::ShaderStageFlagBits::eFragment:
+//            return "FragmentShader_SPIRV";
+//        case vk::ShaderStageFlagBits::eCompute:
+//            return "ComputeShader_SPIRV";
+//        default:
+//            vk::throwResultException(vk::Result::eErrorUnknown, "Unknown shader type");
+//        }
+//    };
+//
+//    std::unique_ptr<FILE, decltype(&fclose)> pFile(fopen(fileName.c_str(), "w"), fclose);
+// 
+//
+//    fprintf(pFile.get(), "static constexpr uint32_t %s[] =", GetShaderStage(stage));
+//    fprintf(pFile.get(), "\n{", GetShaderStage(stage));
+//
+//    for (size_t index = 0; index < spirv.size() / 4; index++) {
+//        if (index % 8 == 0)
+//            fprintf(pFile.get(), "\n    ");
+//        fprintf(pFile.get(), "0x%08x,", spirv[index]);      
+//    }
+// 
+//    fprintf(pFile.get(), "\n};\n", GetShaderStage(stage));
+//}
+//
+
 #undef main
 int main(int argc, char* argv[]) {
     
-    
+
     auto const WINDOW_TITLE = "Application Vulkan";
     auto const WINDOW_WIDTH  = 1920;
     auto const WINDOW_HEIGHT = 1280;
@@ -470,6 +777,7 @@ int main(int argc, char* argv[]) {
         auto const spirvVS = compiler.compileFromFile(L"content/shaders/WaveFront.hlsl", L"VSMain", vk::ShaderStageFlagBits::eVertex, {});
         auto const spirvFS = compiler.compileFromFile(L"content/shaders/WaveFront.hlsl", L"PSMain", vk::ShaderStageFlagBits::eFragment, {});
      
+
         vk::UniqueShaderModule vs = pDevice->createShaderModuleUnique(vk::ShaderModuleCreateInfo{ .codeSize = std::size(spirvVS), .pCode = reinterpret_cast<const uint32_t*>(std::data(spirvVS)) });
         vk::UniqueShaderModule fs = pDevice->createShaderModuleUnique(vk::ShaderModuleCreateInfo{ .codeSize = std::size(spirvFS), .pCode = reinterpret_cast<const uint32_t*>(std::data(spirvFS)) });
         vkx::setDebugName(*pDevice, *vs, "[VS] WaveFront");
